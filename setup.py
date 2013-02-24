a5c37c9cb61f6e5ccfebeebb95a48d020810411b
Jon Eisen <jeisen@underarmour.com>
2013-02-24 11:36:47 -0500

    Added url and increased version to 1.0. Uploaded to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ assert sys.version_info >= (3,), 'pycodejam is a python 3 compatible library.'
 
 setup(
     name = "pycodejam",
-    version = "1.0.0",
+    version = "1.0.1",
     packages = find_packages('src', exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     package_dir = { '': 'src' },
     test_suite = 'codejam.tests',
@@ -14,6 +14,7 @@ setup(
     author = "Jon Eisen",
     author_email = "jon.m.eisen@gmail.com",
     description = "This module provides helpers to run and parse CodeJam problems",
+    url = "http://github.com/yanatan16/pycodejam",
     license = "MIT",
     keywords = "google code jam codejam competition problem",
 )
